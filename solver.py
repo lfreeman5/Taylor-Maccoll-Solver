b0c59587ad8eb198711d19e4792fa8efbf49c8f8
lfreeman5 <122315086+lfreeman5@users.noreply.github.com>
2023-11-28 16:32:51 -0700

    Working on fixing taylor-maccoll solver

--- solver.py
+++ solver.py
@@ -40,23 +40,23 @@ def solveConeAngle(beta, mach, gamma = 1.4):
     v_theta_values = results.T[1]
 
     # Plot v_r and v_theta against theta_values [Disabled for speed]
-    plt.figure(figsize=(8, 6))
-    plt.plot(theta_values, v_r_values, label='v_r')
-    plt.plot(theta_values, v_theta_values, label='v_theta')
-    plt.xlabel('Theta Values')
-    plt.ylabel('Velocity')
-    plt.title(f'v_r and v_theta vs Theta Values\nM{mach} Beta {beta}')
-    plt.legend()
-    plt.ylim(-0.4, 0.4)
-    plt.grid(True)
-    plt.show()
+    # plt.figure(figsize=(8, 6))
+    # plt.plot(theta_values, v_r_values, label='v_r')
+    # plt.plot(theta_values, v_theta_values, label='v_theta')
+    # plt.xlabel('Theta Values')
+    # plt.ylabel('Velocity')
+    # plt.title(f'v_r and v_theta vs Theta Values\nM{mach} Beta {beta}')
+    # plt.legend()
+    # plt.ylim(-0.4, 0.4)
+    # plt.grid(True)
+    # plt.show()
 
 
     # Find the index where v_theta changes sign
     change_point_index = np.where(np.diff(np.sign(v_theta_values)))[0][0]
     # Print the corresponding theta value
     theta_change_point = 1/3 * (theta_values[change_point_index] + theta_values[change_point_index+1] + theta_values[change_point_index-1])
-    # print(f'For M={mach} and B = {np.degrees(beta)}, the theta value where v_theta changes sign: {np.degrees(theta_change_point)}')
+    # print(f'For M={mach} and B = {np.degrees(beta)}, the cone angle is: {np.degrees(theta_change_point)}')
 
     v_r_f = v_r_values[change_point_index]
     m_surf = np.sqrt((2/(gamma-1)) * (1/(1/(v_r_f**2) - 1)))
@@ -66,38 +66,51 @@ def solveConeAngle(beta, mach, gamma = 1.4):
 def findShockParameters(theta, mach, gamma=1.4):
     #Paramters: theta is the cone angle, mach is freestream mach number
     #Pass theta in radians, mach dimensionless
-    angles = np.linspace(np.pi/2, 0, 400) #Deflection from 90 degrees to zero 
+    angles = np.linspace(np.pi/2, 0, 1000) #Deflection from 90 degrees to zero 
     coneAngle = -1
+    goingDown = False
+    prevTheta = -1
+    coneAngle, surfaceMach = 0,0
+    beta=0
     for angle in angles: #Goes from 90 to zero
         #Run solveConeAngle with beta, if it fails then it didnt' converge to a solution. The first time it converges, great.
         try:
-            print('Trying angle ', angle, 'at M=', mach)
+            # print('Trying angle ', np.degrees(angle), 'at M=', mach)
             coneAngle, surfaceMach = solveConeAngle(angle, mach)
-            if coneAngle < theta: #The cone angles will start large because we start at a deflection of 90 degrees
+            if not goingDown and coneAngle < prevTheta:
+                # print("Descending")
+                goingDown = True 
+            
+            if coneAngle < theta and goingDown: #The cone angles will start large because we start at a deflection of 90 degrees
+                beta = angle
                 break
         except IndexError as e:
             print(f"Error: {e}")
             continue
+        prevTheta = coneAngle
     if coneAngle == -1:
         return "NO SOLUTION FOUND"
     else:
         #Using constitutive relationships find surface pressure
         pressureRatio = (1 + surfaceMach**2 * (gamma - 1)/2) ** (gamma / (gamma - 1))
+        pressureCoefficient = 2/(gamma*mach**2) * (pressureRatio-1)
         print("Surface Mach Number: ", surfaceMach)
+        print("Shock Angle: ", np.degrees(beta))
         print("Surface Pressure Ratio for cone: ", pressureRatio)
+        print("Surface Pressure Coefficient for cone: ", pressureCoefficient)
         return
     
 
-# solveConeAngle(np.radians(45), 2.2)
+# solveConeAngle(np.radians(45), 5)
+# solveConeAngle(np.radians(40), 5)
+# solveConeAngle(np.radians(35), 5)
 # solveConeAngle(np.radians(30), 5)
-# solveConeAngle(np.radians(40), 4)
-# solveConeAngle(np.radians(20), 14)
-print(solveConeAngle(1.5, 21))
-print(solveConeAngle(1.4, 21))
-print(solveConeAngle(1.3, 21))
-print(solveConeAngle(1.2, 21))
-print(solveConeAngle(1.1, 21))
-# findShockParameters(np.radians(25), 32)
+# print(solveConeAngle(1.5, 21))
+# print(solveConeAngle(1.4, 21))
+# print(solveConeAngle(1.3, 21))
+# print(solveConeAngle(1.2, 21))
+# print(solveConeAngle(1.1, 21))
+findShockParameters(np.radians(10), 5)
 # solveConeAngle(np.radians(90), 2.2)
 
 
@@ -109,7 +122,7 @@ print(solveConeAngle(1.1, 21))
 # plt.legend()
 # plt.show()
 
-# ###Plotting oblique shock relations
+###Plotting oblique shock relations
 # mach_numbers = [2, 3, 4, 5, 10]
 # beta_range = np.radians(np.arange(0, 91, 0.1))  # Convert beta to radians
 
@@ -118,11 +131,11 @@ print(solveConeAngle(1.1, 21))
 #     deflection_results = [np.degrees(oblique_shock_relations(beta, mach_number)[0]) for beta in beta_range]
 
 #     # Filter data for deflection > 0
-#     beta_filtered = [np.degrees(beta) for i, beta in enumerate(beta_range) if deflection_results[i] > 0]
-#     deflection_filtered = [deflection for deflection in deflection_results if deflection > 0]
+#     # beta_filtered = [np.degrees(beta) for i, beta in enumerate(beta_range) if deflection_results[i] > 0]
+#     # deflection_filtered = [deflection for deflection in deflection_results if deflection > 0]
 
 #     # Plot the filtered results with flipped axes
-#     plt.plot(deflection_filtered, beta_filtered, label=f'Mach {mach_number}')
+#     plt.plot(deflection_results, beta_range, label=f'Mach {mach_number}')
 
 # plt.title('Oblique Shock Deflection Angle vs. Beta')
 # plt.xlabel('Deflection Angle (degrees)')
