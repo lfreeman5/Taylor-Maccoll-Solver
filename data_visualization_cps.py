71bc195dccf58f9b86bdb61e71aa2f195f8d291d
lfreeman5 <122315086+lfreeman5@users.noreply.github.com>
2023-12-11 17:43:50 -0700

    Update visualization, EQ references

--- data_visualization_cps.py
+++ data_visualization_cps.py
@@ -32,12 +32,12 @@ for mach, m_dict in data.items():
     cps = m_dict['cps']
     ax.scatter(np.ones(len(thetas))*mach, thetas, cps, label=f'M {mach}')
 
-coefficients = create_coeffs('results_CP.json', 'cps')
+coefficients = create_coeffs('results_Big.json', 'cps', orders=[2,3,6])
 
 gen_thetas = np.linspace(0,1,30)
 gen_machs = np.linspace(1,30,30)
 gen_thetas, gen_machs = np.meshgrid(gen_thetas, gen_machs)
-gen_cps = evaluate_function(coefficients, [1.4*np.ones_like(gen_thetas), gen_machs, gen_thetas])
+gen_cps = evaluate_function(coefficients, [1.4*np.ones_like(gen_thetas), gen_machs, gen_thetas], orders=[2,3,6])
 ax.contour3D(gen_machs, gen_thetas, gen_cps, 50, cmap='binary')
 
 ax.set_xlabel('Mach Number')
@@ -53,7 +53,7 @@ for mach, m_dict in data.items():
     print(mach)
     thetas = m_dict['thetas']
     cps = m_dict['cps']
-    error = (cps - evaluate_function(coefficients, [np.ones(len(thetas))*1.4, np.ones(len(thetas))*mach, np.array(thetas)]))*100
+    error = (cps - evaluate_function(coefficients, [np.ones(len(thetas))*1.4, np.ones(len(thetas))*mach, np.array(thetas)], orders=[2,3,6]))*100
     ax.scatter(np.ones(len(thetas))*mach, thetas, error)
 
 ax.set_xlabel('Mach Number')
